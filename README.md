c13ebfe0cb5944b7b01bf89b0342ceb325e5e327
AJ Jones <ajjonez@icloud.com>
2017-10-22 19:57:41 -0500

    refactored code after review

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # Pixel Art Maker Project
 
-## Table of Contents
+## Project for Udacity
 
 * [Instructions](#instructions)
 * [Contributing](#contributing)
 
-## Instructions
+This is my first go at an "app" that is fully functional. I tried to ensure to use event methods like .click().
 
-To get started, open `designs.js` and start building out the app's functionality.
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+Enjoy.
--- designs.js
+++ designs.js
@@ -1,8 +1,20 @@
 
 const h = $('#input_height');
 const w = $('#input_width');
-
 let color  = $('#colorPicker');
+
+function makeGrid() {
+	let height = h.val();
+	let width  = w.val();
+	$('#pixel_canvas').empty();
+	
+	for(let row = 0; row < height; row++) {
+		$('#pixel_canvas').append('<tr></tr>');
+		for (let col = 0; col < width; col++) {
+			$('tr').last().append('<td></td>');
+		}
+	}
+}; 
 
 $('#sizePicker').submit(function(e){
 	let height = h.val();
@@ -12,19 +24,8 @@ $('#sizePicker').submit(function(e){
 
 	makeGrid();
 
-	function makeGrid() {
-		$('#pixel_canvas').empty();
-		for(let row = 0; row < width; row++) {
-			$('#pixel_canvas').append('<tr></tr>');
-			for (let col = 0; col < height; col++) {
-				$('tr').last().append('<td></td>');
-			}
-		}
-	};
+	$('td').click(function(){
+		$(this).css('backgroundColor', $(color).val());
+	});
 
-  $('td').click(function(){
-    $(this).css('backgroundColor', $(color).val());
-  });
 });
-
-
