fd609905d371402be446d555102e27849e7e8b6e
AJ Jones <ajjonez@icloud.com>
2017-10-23 22:30:05 -0500

    adding event delegation

--- designs.js
+++ designs.js
@@ -1,15 +1,15 @@
 
-const heightInput = $('#input_height');
-const widthInput = $('#input_width');
-const colorInput  = $('#colorPicker');
-const canvas = $('#pixel_canvas');
+const $heightInput = $('#input_height');
+const $widthInput = $('#input_width');
+const $colorInput  = $('#colorPicker');
+const $canvas = $('#pixel_canvas');
 
 /**
  * this function will all us to create a grid
  */
 function makeGrid(height,width) {
 	/** let's clean the canvas on every submit */
-	canvas.empty();
+	$canvas.empty();
 	/**  here we'll loop through each row and col */
 	for (let row = 0; row < height; row++) {
 		$('#pixel_canvas').append('<tr></tr>');
@@ -23,8 +23,8 @@ function makeGrid(height,width) {
  * is pushed so we can add color from the color picker
  */
 $('#sizePicker').submit(function(e){
-	let height = heightInput.val();
-	let width  = widthInput.val();
+	let height = $heightInput.val();
+	let width  = $widthInput.val();
 	/**
 	 * With the event listener tied to the submit method
 	 * we can listen for a new click and clear the grid
@@ -35,11 +35,11 @@ $('#sizePicker').submit(function(e){
 	 * Here we call the grid function with the submit button
 	 */
 	makeGrid(height,width);
-	/** 
-	 * Using the click method we can add color
-	 * to each grid box from the color selector
-	 */
-	$('td').click(function(){
-		$(this).css('backgroundColor', colorInput.val());
-	});
+});
+/** 
+ * Using the click method we can add color
+ * to each grid box from the color selector
+ */
+$canvas.on('click', 'td', function(){
+	$(this).css('backgroundColor', $colorInput.val());
 });
